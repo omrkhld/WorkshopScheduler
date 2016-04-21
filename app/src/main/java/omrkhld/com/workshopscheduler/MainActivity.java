510912fac030fa7da6f8bcbe38dd4d548228acc5
Omar <omar.khalid.yahya@gmail.com>
2016-04-21 15:57:00 +0800

    Filter by services done.

--- app/src/main/java/omrkhld/com/workshopscheduler/MainActivity.java
+++ app/src/main/java/omrkhld/com/workshopscheduler/MainActivity.java
@@ -1,14 +1,13 @@
 package omrkhld.com.workshopscheduler;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.os.WorkSource;
-import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -29,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     public ArrayList<Workshop> workshops = null;
     private CardAdapter ca;
+    public AlertDialog dialog;
+    public final CharSequence[] filters = {" Tyre Change "," Oil Change "," Battery Change "};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -133,6 +134,97 @@ public class MainActivity extends AppCompatActivity {
         return ca;
     }
 
+    public void showFilterDialog() {
+        final ArrayList<Integer> selectedFilters = new ArrayList<>();
+        final ArrayList<Workshop> filteredShops = new ArrayList<Workshop>();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add Filters");
+        builder.setMultiChoiceItems(filters, null,
+                new DialogInterface.OnMultiChoiceClickListener() {
+                    // indexSelected contains the index of item (of which checkbox checked)
+                    @Override
+                    public void onClick(DialogInterface dialog, int indexSelected, boolean isChecked) {
+                        if (isChecked) {
+                            // If the user checked the item, add it to the selected filters
+                            selectedFilters.add(indexSelected);
+                        } else if (selectedFilters.contains(indexSelected)) {
+                            // Else, if the item is already in the array, remove it
+                            selectedFilters.remove(Integer.valueOf(indexSelected));
+                        }
+                    }
+                })
+                // Set the action buttons
+                .setPositiveButton("Filter", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        if (selectedFilters.size() == 1) {
+                            int index = selectedFilters.get(0);
+                            if (index == 0) { // Tyre change
+                                for (int j = 0; j < workshops.size(); j++) {
+                                    Workshop w = workshops.get(j);
+                                    if (w.getTyreChange() && !filteredShops.contains(w)) {
+                                        filteredShops.add(w);
+                                    }
+                                }
+                            } else if (index == 1) { // Oil change
+                                for (int j = 0; j < workshops.size(); j++) {
+                                    Workshop w = workshops.get(j);
+                                    if (w.getOilChange() && !filteredShops.contains(w)) {
+                                        filteredShops.add(w);
+                                    }
+                                }
+                            } else if (index == 2) { // Battery change
+                                for (int j = 0; j < workshops.size(); j++) {
+                                    Workshop w = workshops.get(j);
+                                    if (w.getBattChange() && !filteredShops.contains(w)) {
+                                        filteredShops.add(w);
+                                    }
+                                }
+                            }
+                        } else if (selectedFilters.contains(0) && selectedFilters.contains(1) && !selectedFilters.contains(2)) {
+                            for (int j = 0; j < workshops.size(); j++) {
+                                Workshop w = workshops.get(j);
+                                if (w.getTyreChange() && w.getOilChange() && !filteredShops.contains(w)) {
+                                    filteredShops.add(w);
+                                }
+                            }
+                        } else if (selectedFilters .contains(0) && !selectedFilters.contains(1) && selectedFilters.contains(2)) {
+                            for (int j = 0; j < workshops.size(); j++) {
+                                Workshop w = workshops.get(j);
+                                if (w.getTyreChange() && w.getBattChange() && !filteredShops.contains(w)) {
+                                    filteredShops.add(w);
+                                }
+                            }
+                        } else if (!selectedFilters .contains(0) && selectedFilters.contains(1) && selectedFilters.contains(2)) {
+                            for (int j = 0; j < workshops.size(); j++) {
+                                Workshop w = workshops.get(j);
+                                if (w.getOilChange() && w.getBattChange() && !filteredShops.contains(w)) {
+                                    filteredShops.add(w);
+                                }
+                            }
+                        } else if (selectedFilters .contains(0) && selectedFilters.contains(1) && selectedFilters.contains(2)) {
+                            for (int j = 0; j < workshops.size(); j++) {
+                                Workshop w = workshops.get(j);
+                                if (w.getTyreChange() && w.getOilChange() && w.getBattChange() && !filteredShops.contains(w)) {
+                                    filteredShops.add(w);
+                                }
+                            }
+                        }
+                        ca = updateAdapter(filteredShops);
+                        mRecyclerView.setAdapter(ca);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+
+        dialog = builder.create();//AlertDialog dialog; create like this outside onClick
+        dialog.show();
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -144,6 +236,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.filter:
+                showFilterDialog();
                 return true;
             case R.id.map:
                 startActivity(new Intent(this, MapsActivity.class));
